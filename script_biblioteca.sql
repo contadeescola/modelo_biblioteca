3a866b8172e7277507030db6876e4e18db2b600b
contadeescola <160253002+contadeescola@users.noreply.github.com>
2024-06-21 11:52:53 -0300

    adicionei mas duas tabelas

--- script_biblioteca.sql
+++ script_biblioteca.sql
@@ -3,7 +3,20 @@ create database biblioteca;
 use biblioteca;
 
 create table professor (
+pk int,
 nome varchar(255),
-cpf varchar(45),
-pk int
+cpf varchar(45)
+);
+
+create table sala (
+pk int,
+quantidade_aluno int(4),
+numero_sala varchar(4),
+professor_fk int,
+turma_fk int
+);
+
+create table turma (
+pk int,
+nome varchar(255)
 );
